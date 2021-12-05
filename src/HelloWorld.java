dfe4186d2b989f9ba51e524d4b434df36e4623a2
Sange0104 <sange.pp@gmail.com>
2021-12-05 17:54:43 -0500

    Change in code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 	
-		//System.out.println("Hello, World!");
+		System.out.println("Hello, World!");
 
-		System.out.println("Hello Sangeetha");
+		//System.out.println("Hello Sangeetha");
 	} //End of Main method
 
 } //End of Class
